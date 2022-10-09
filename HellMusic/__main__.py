adae82c6b8ef2bc359bdd0d238311659f0cda6b8
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-10-09 15:52:30 +0530

    Update __main__.py

--- HellMusic/__main__.py
+++ HellMusic/__main__.py
@@ -8,29 +8,32 @@ from HellMusic.core.plugins import load_plugins
 from HellMusic import bot, hell, client, helldb, __version__
 
 
-loop = asyncio.get_event_loop()
-
-
 async def startup():
     if not Config.HELLBOT_SESSION:
         LOGS.error("[HELLBOT_SESSION]: Not a valid session!")
         sys.exit()
 
-    LOGS.info("••• Music Bot Startup •••")
+    try:
+        LOGS.info("••• Music Bot Startup •••")
+        await bot.start()
+        await client.start()
+        await hell.start()
+        await load_plugins()
+        if Config.DB_URI:
+            LOGS.info("••• Database Url Found •••")
+            await helldb.get_db()
+        LOGS.info(DEPLOYED.format(__version__.version))
+        await idle()
+    except Exception as e:
+        LOGS.error(str(e))
 
-    await bot.start()
-    await client.start()
-    await hell.start()
-    await load_plugins()
 
-    if Config.DB_URI:
-        LOGS.info("••• Database Url Found •••")
-        await helldb.get_db()
+client.loop.run_until_complete(startup())
 
-    LOGS.info(DEPLOYED.format(__version__.version))
-    await idle()
-
-
-if __name__ == "__main__":
-    loop.run_until_complete(startup())
-    LOGS.info("••• Hell-Music Stopped •••")
+if len(sys.argv) not in (1, 3, 4):
+    client.disconnect()
+else:
+    try:
+        client.run_until_disconnected()
+    except ConnectionError:
+        pass
